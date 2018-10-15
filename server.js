6f3a7626e1f282d1ee1c604b9e0a7747bb4fc26e
zp25 <zebrap25@gmail.com>
2018-10-15 23:46:07 +0800

    feat: 更新server，移除favicon，更新helmet

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const favicon = require('serve-favicon');
 const compression = require('compression');
 const errorHandler = require('errorhandler');
 const helmet = require('helmet');
@@ -14,7 +13,12 @@ const scripts = path.resolve(static, 'scripts');
 const styles = path.resolve(static, 'styles');
 
 app.set('port', process.env.PORT || 3001);
-app.set('favicon', path.resolve(static, 'images/favicon.png'));
+
+const FAVICON = [
+  'favicon.png',
+  'icon@iphone.png',
+  'icon@iphone-plus.png',
+];
 
 // Use Helmet
 app.disable('x-powered-by');
@@ -22,29 +26,44 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
+      workerSrc: ["'self'"],
+      frameSrc: ["'none'"],
       objectSrc: ["'none'"],
       frameAncestors: ["'none'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
     },
     browserSniff: false,
   },
+  dnsPrefetchControl: {
+    allow: false,
+  },
+  // expectCt: { maxAge: 123 },
   frameguard: {
     action: 'deny',
   },
+  // hpkp: { maxAge: 123 },
   hsts: {
-    maxAge: ms('0.5y') / 1000,
+    maxAge: ms('2y') / 1000,
     includeSubDomains: true,
+    preload: true,
   },
+  ieNoOpen: true,
+  // noCache: false,
+  noSniff: true,
   referrerPolicy: {
-    policy: 'origin-when-cross-origin',
+    policy: 'strict-origin-when-cross-origin',
   },
+  xssFilter: true,
 }));
 
 // middleware
 app.use(compression());
-app.use(favicon(app.get('favicon')));
 
 // static assets
+FAVICON.forEach((icon) => {
+  app.use(`/images/${icon}`, express.static(path.resolve(static, 'images', icon), { maxAge: ms('0.5y') }));
+});
+
 app.use('/images', express.static(images, { maxAge: ms('1w') }));
 app.use('/scripts', express.static(scripts, { maxAge: ms('0.5y') }));
 app.use('/styles', express.static(styles, { maxAge: ms('0.5y') }));
